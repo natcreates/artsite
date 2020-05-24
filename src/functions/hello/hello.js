be27bc4693be92bcba8eee00d0b00427339f6513
natcreates <natalie.dixon@tesglobal.com>
2020-05-24 12:31:07 +0100

    Get carousel working

--- src/functions/hello/hello.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (image, position, images) => {
-  try {
-    const img = document.getElementById('easel');
-    img.src = ""
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
-};
